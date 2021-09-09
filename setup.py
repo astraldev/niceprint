5d5472cd93d9f5366932b9c8ed72e19a7ceb4e08
astraldev <61796351+astraldev@users.noreply.github.com>
2021-09-09 04:00:21 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
+import pathlib
+import os
 
+here = pathlib.Path(__file__).parent
+os.chdir(here)
 setup(
-    name="niceprint",
-    version='1.3.0',
-    author="AstralDev",
-    author_email="ekureedem480@gmail.com",
-    description="A simple module for print.",
-    long_description_content_type="text/markdown",
-    long_description=str(open("README.md").read()),
-    license="MIT",
-    py_modules=["niceprint"],
-    install_requires=["color"],
+  name="niceprint",
+  version="3.0.0",
+  license="GNU GPL 3",
+  url="http://astraldev.github.io/niceprint",
+  description="A minute package for formating output",
+  long_description=str(open('README.md').read()),
+  long_description_content_type="text/markdown",
+  author="AstralDev",
+  author_email="ekureedem480@gmail.com",
+  python_requires='>=3',
+  install_requires=["json"],
+  py_modules=["niceprint"]
 )
